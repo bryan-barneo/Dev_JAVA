abcd1d53f9e05be89415ed425fbae98584a8770a
barneob <bryan.barneo@etu.umontpellier.fr>
2023-01-03 15:28:04 +0100

    JAVA

--- Date/src/Date.java
+++ Date/src/Date.java
@@ -5,27 +5,31 @@ public class Date {
     private int jour;
     private int mois;
     private int année;
-    private static String[] moisLettres = {"janvier", "février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre"};
+    private static String[] moisLettres = {"janvier", "février", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "décembre"};
+    private String MoisLettre = "";
     public Date(int J, int M, int A){
         this.jour = J;
         this.mois = M;
         this.année = A;
     }
-    public String intEnLettres(moisLettres){
+    public String intEnLettres(){
         String[] moisLettre = this.moisLettres;
         String Mois = "";
         int Moisint = this.mois;
         for(int i = 0; i <= Moisint-1; i++){
-            if(Moisint == i){
-                Mois += moisLettres;
+            if(Moisint-1 == i){
+                Mois += moisLettre[i];
+                this.MoisLettre = Mois;
             }
+
         }
         return Mois;
     }
     public String toString() {
-        return this.jour + "/" + this.mois + "/" + année;
+        intEnLettres();
+        return this.jour + " " + this.MoisLettre + " " + année;
     }
     public void Affiche(){
-        System.out.println("La date rentré est : Le " + this.jour +"/"+ this.mois + "/" + this.année);
+        System.out.println("La date rentré est : Le " + this.jour +" "+ this.mois + " " + this.année);
     }
 }
--- Date/src/MainDate.java
+++ Date/src/MainDate.java
@@ -1,8 +1,14 @@
+import java.sql.SQLOutput;
+
 public class MainDate {
     public static void main(String[] args) {
-        Date date1, date2, date3;
-        date1 = new Date(02,12,2022);
-        date1.Affiche();
-        date1.intEnLettres();
+        Date d = new Date(21,12,2022);
+        Date e = new Date(d);
+        if (e.toString().equals(d.toString())) {
+            System.out.println("1");
+        }
+        else {
+            System.out.println("0");
+        }
     }
 }
